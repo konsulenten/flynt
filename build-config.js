9f599798fbcb803b7ae707598b66a364d9189f39
konsulenten <konsulenten@users.noreply.github.com>
2023-01-12 09:47:28 +0100

    Initial master

--- build-config.js
+++ build-config.js
@@ -1,4 +1,4 @@
-const host = 'https://flynt.test'
+const host = 'http://localhost:10038/'
 const dest = './dist'
 
 function getCopyConfig (source) {
@@ -53,7 +53,7 @@ module.exports = {
       './Components/**/*.{php,twig}'
     ],
     watch: true,
-    https: true
+    https: false
   },
   webpackDevMiddleware: {
     stats: false,
